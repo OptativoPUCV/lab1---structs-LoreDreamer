2445ff22869e3b52c950e19a65df083ff61a392e
Your Name <you@example.com>
2024-03-13 23:37:26 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -85,13 +85,36 @@ arreglos en un tercer arreglo también ordenado.
 */
 
 
-void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
-                       int result[]) {
+void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2, int result[]) {
 
+  int newSize = size1 + size2;
+
+  for (size_t i = 0; i < newSize; i++) {
+
+    if (i < size1) 
+      result[i] = arr1[i];
+    else
+    result[i] = arr2[i - size1];
+                         
+  }
+
+  for (size_t i = 0; i < newSize - 1; i++) {
+    for (size_t j = 0; j < newSize - i - 1; j++) {
+
+      if (result[j] > result[j + 1]) {
+        
+        int tempItem = result[j];
+        result[j] = result[j+1];
+        result[j+1] = tempItem;
+      
+      }
+    }
+  }
+                        
 
 
   
-                       }
+}
 
 /*
 Ejercicio 5: Comprobación de Ordenación
